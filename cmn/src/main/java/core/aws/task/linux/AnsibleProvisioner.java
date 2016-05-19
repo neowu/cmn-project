fb205c63e9bc6d67a176532c72c7b1cf075963b8
neo <neowu.us@gmail.com>
2016-05-19 15:35:24 -0400

    update ansible provisioner to support Ubuntu16.04

--- cmn/src/main/java/core/aws/task/linux/AnsibleProvisioner.java
+++ cmn/src/main/java/core/aws/task/linux/AnsibleProvisioner.java
@@ -48,7 +48,7 @@ public class AnsibleProvisioner {
         logger.info("packageDir => {}", packageDir);
 
         try (SSH ssh = new SSH(hostName(instance), "ubuntu", KeyPair.keyFile(instance.getKeyName(), env))) {
-            ssh.executeCommands("dpkg -s ansible || ([[ $(lsb_release -r) =~ '14.04' ]] && sudo apt-add-repository -y ppa:ansible/ansible && sudo apt-get -y -q update && sudo apt-get -y install ansible)",
+            ssh.executeCommands("dpkg -s ansible || ([[ $(lsb_release -r) =~ '14.04' ]] && sudo apt-add-repository -y ppa:ansible/ansible && sudo apt-get -y -q update && sudo apt-get -y install ansible) || echo not ubuntu 14.04",
                 "dpkg -s ansible || (sudo apt-get -y -q update && sudo apt-get -y install ansible)",
                 "sudo rm -rf /opt/ansible /opt/packages",    // clear previous ansible roles and packages if installed
                 "sudo mkdir -p /opt/packages /opt/ansible",
